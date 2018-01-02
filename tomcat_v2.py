489d7264effe217caa5096b1b9636dfb4a540bc2
hpf <hpf1992@126.com>
2018-01-01 20:52:21 -0500

    add rollback

--- tomcat_v2.py
+++ tomcat_v2.py
@@ -6,13 +6,16 @@ from fabric.context_managers import *
 from fabric.contrib.console import confirm
 import sys
 import time
+import os
+import hashlib
+import re
 
 env.user='root'
-env.hosts=['192.168.1.182']
+env.hosts=['192.168.1.151']
 env.local_path='/root/project/'
 env.remote_base_path='/root/project'
 env.remote_dir='/opt/tomcat'
-env.dir_dict={'backend':'webapps_bd', 'h5pay':'webapps_h5_pay'}
+env.dir_dict={'backend':'webapps_bd', 'h5pay':'webapps_h5_pay', 'posmerchant':'webapps_pos'}
 
 pack = raw_input('请输入包名: ').strip()
 print pack
@@ -36,7 +39,36 @@ def put_package():
         print(yellow('开始上传。。。'))
         time.sleep(3)
         put(red_war, remote_dir)
-#        print env.dir_dict[pg_prefix]
+
+@task
+def roll_back():
+    print(yellow('回滚版本！'))
+    with cd(remote_dir):
+        global rollback_ver
+        rollback_ver = run("ls -l last_version | awk '{print $NF}'")
+        print 'name:', rollback_ver
+
+@task
+def backup():
+    print(yellow('备份...'))
+    with cd(app_dir):
+        rootmd5 = run('md5sum %s' % 'ROOT.war').split(' ')[0]
+        print '====================================================='
+    with cd(remote_dir):
+        print 'rootmd5', rootmd5
+        a = run("for I in $(ls -l | awk 'NR!=1{print $NF}'); do md5sum $I ; done")
+        print a
+        if rootmd5  in a:
+            print 'YES' 
+            last_ver_list = re.findall(r'%s.*war' % rootmd5, a)
+            print last_ver_list
+            last_ver = last_ver_list[0]
+            b = last_ver.split('  ')[1]
+            run('[ -e last_version ] && rm -f last_version || /bin/true')
+            run('ln -sv %s last_version' % b)
+        else:
+            print 'NO'
+            sys.exit('没有备份')
 
 @task
 def pack_validate():
@@ -51,9 +83,12 @@ def pack_validate():
     else:
         print(red('文件上传错误，请重试！'))
         sys.exit()
+
+@task
+def deploy(war):
     with cd(app_dir):
         run('rm -rf ROOT*')
-        root_war = '%s%s' %(remote_dir, red_war)
+        root_war = '%s%s' %(remote_dir, war)
         print root_war
         run('cp %s ./ROOT.war' % root_war) 
     print(yellow('部署成功！准备重启服务。。。'))
@@ -63,6 +98,7 @@ def restart_serv():
     print(red('重启tomcat!'))
     with settings(warn_only=True):
         result = run('/etc/init.d/tomcat stop')
+#        result = run('set -m;/opt/tomcat/bin/shutdown.sh')
     if result.failed and not confirm('Command failed.Continue anyway?'):
         abort('Aborting at user request.')
     time.sleep(3)
@@ -70,6 +106,15 @@ def restart_serv():
 
 @task
 def production():
+    backup()
     put_package()
     pack_validate()
+    deploy(red_war)
+    restart_serv()
+
+@task
+def rollback():
+    roll_back()
+    backup()
+    deploy(rollback_ver)
     restart_serv()
